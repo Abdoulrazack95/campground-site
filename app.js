2bec89af71f1bf5a55b843ac28dd0535947d3f00
Abdoulrazack <41553365+Abdoulrazack95@users.noreply.github.com>
2019-08-27 20:01:37 +0100

    configuring Authenticate passwort done

--- app.js
+++ app.js
@@ -2,20 +2,37 @@ var express                 = require("express"),
     app                     = express(),
     bodyParser              = require("body-parser"),
     mongoose                = require("mongoose"),
-    Campground              = require("./models/campground"),
-    seedDB                  = require("./seeds"),
+    session                 = require("express-session"),
     passport                = require("passport"),
     LocalStrategy           = require("passport-local"),
-    passportLocalMongoose   = require("passport-local-mongoose");
+    passportLocalMongoose   = require("passport-local-mongoose"),
+    Campground              = require("./models/campground"),
+    seedDB                  = require("./seeds"),
+    User                    = require("./models/user"),
     Comment                 = require("./models/comment");
 
 mongoose.connect('mongodb://localhost:27017/campyeah', { useNewUrlParser: true });
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
-console.log(__dirname );
+app.use(session({
+    secret: "I am clever and I know it",
+    resave: false,
+    saveUninitialized: false
+}))
 seedDB();
+
+//PASSPORT CONFIGURATION
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+
+//Read the sesion and collecte the data in order to encode and to decode.
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.redirect("/campgrounds");
@@ -94,6 +111,21 @@ app.post("/campgrounds/:id/comments", function(req, res){
         }
     })
 })
+
+
+//=====================
+//AUTHENTICATE ROUTERS
+//=====================
+
+//SHOW regiter form
+
+// app.get("/register", (req, res) => {
+//     res.render("register");
+// })
+
+// app.post("/register", (req, res) => {
+    
+// })
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server has started");
